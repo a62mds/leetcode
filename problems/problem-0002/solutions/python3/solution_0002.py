55fff4df040bdcf7de5399b9d176fae174502fe2
Mitch Sullivan <sullivan.mitch@gmail.com>
2022-03-21 13:25:27 -0230

    Add Python implementation of solution

--- problems/problem-0002/solutions/python3/solution_0002.py
+++ problems/problem-0002/solutions/python3/solution_0002.py
@@ -1,6 +1,61 @@
 """
 solution_0002.py
 """
+from __future__ import annotations
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    @classmethod
+    def from_list(cls: ListNode, l: list) -> ListNode:
+        ln = ListNode(l[0])
+        _ln = ln
+        for x in l[1:]:
+            _ln.next = ListNode(x)
+            _ln = _ln.next
+        return ln
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def __eq__(self, other) -> bool:
+        self_ln = self
+        other_ln = other
+        while True:
+            if self_ln.val != other_ln.val:
+                return False
+            if self_ln.next is None:
+                if other_ln.next is not None:
+                    return False
+                else:
+                    break
+            if other_ln.next is None:
+                if self_ln.next is not None:
+                    return False
+                else:
+                    break
+            self_ln = self_ln.next
+            other_ln = other_ln.next
+        return True
+
 
 class Solution:
-    pass
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        result: ListNode = ListNode()
+        current: ListNode = result
+        p: Optional[ListNode] = l1
+        q: Optional[ListNode] = l2
+        carry: int = 0
+        while p or q:
+            sum: int = (p.val if p else 0) + (q.val if q else 0) + carry
+            carry = sum // 10
+            sum = sum % 10
+            current.next = ListNode(sum)
+            current = current.next
+            if p:
+                p = p.next
+            if q:
+                q = q.next
+        if carry:
+            current.next = ListNode(carry)
+        return result.next
--- problems/problem-0002/solutions/python3/test_solution_0002.py
+++ problems/problem-0002/solutions/python3/test_solution_0002.py
@@ -1,13 +1,33 @@
-from typing import List
 import unittest
 
-from solution_0002 import Solution
+from solution_0002 import ListNode, Solution
 
 
 class TestSolution0002(unittest.TestCase):
 
     def setUp(self):
         self.solution = Solution()
+
+    def test_sum_of_243_and_564(self):
+        input1: ListNode = ListNode.from_list([2, 4, 3])
+        input2: ListNode = ListNode.from_list([5, 6, 4])
+        output: ListNode = ListNode.from_list([7, 0, 8])
+
+        self.assertEqual(self.solution.addTwoNumbers(input1, input2), output)
+
+    def test_sum_of_0_and_0(self):
+        input1: ListNode = ListNode.from_list([0])
+        input2: ListNode = ListNode.from_list([0])
+        output: ListNode = ListNode.from_list([0])
+
+        self.assertEqual(self.solution.addTwoNumbers(input1, input2), output)
+
+    def test_sum_of_9999999_and_9999(self):
+        input1: ListNode = ListNode.from_list([9, 9, 9, 9, 9, 9, 9])
+        input2: ListNode = ListNode.from_list([9, 9, 9, 9])
+        output: ListNode = ListNode.from_list([8, 9, 9, 9, 0, 0, 0, 1])
+
+        self.assertEqual(self.solution.addTwoNumbers(input1, input2), output)
 
 
 if __name__ == "__main__":
